74cefe44e751657ad70eca2aded6dd4ef390a3df
Karine Brandelli <karinebrandelli@live.com>
2022-12-29 11:48:11 -0300

    build: set state for latitude and longitude, set state for map

--- src/App.js
+++ src/App.js
@@ -5,6 +5,19 @@ import { ImLocation } from "react-icons/im";
 import "./App.css";
 
 function App() {
+  const [lat, setLat] = useState(22.5726);
+  const [lon, setLon] = useState(88.3639);
+
+  const [viewport, setViewport] = useState({
+    latitude: lat,
+    longitude: lon,
+    zoom: 12,
+    bearing: 0,
+    pitch: 0,
+    width: "100%",
+    height: "100vh",
+  });
+
   return (
     <div className="App">
 
