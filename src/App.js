03b714cf50e4cbe2139ca93e62dc3e84a9331c36
Karine Brandelli <karinebrandelli@live.com>
2022-12-29 11:34:19 -0300

    build: import react-map-gl, Fly, react-icons  components, create Fly component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
-import './App.css';
+import { useEffect, useState } from "react";
+import ReactMapGL, { Marker, FlyToInterpolator } from "react-map-gl";
+import Fly from "./components/Fly";
+import { ImLocation } from "react-icons/im";
+import "./App.css";
 
 function App() {
   return (
