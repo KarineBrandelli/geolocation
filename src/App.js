4cf097d97fd1db5e6cb7397e85ab3a7da99bfe71
Karine Brandelli <karinebrandelli@live.com>
2022-12-29 13:40:41 -0300

    docs: return to basic docs

--- src/App.js
+++ src/App.js
@@ -1,40 +1,11 @@
-import { useEffect, useState } from "react";
-import ReactMapGL, { Marker, FlyToInterpolator } from "react-map-gl";
-import Fly from "./components/Fly";
-import { ImLocation } from "react-icons/im";
 import "./App.css";
 
 function App() {
-  const [lat, setLat] = useState(22.5726);
-  const [lon, setLon] = useState(88.3639);
-
-  const [viewport, setViewport] = useState({
-    latitude: lat,
-    longitude: lon,
-    zoom: 12,
-    bearing: 0,
-    pitch: 0,
-    width: "100%",
-    height: "100vh",
-  });
-
-  useEffect(() => {
-    setViewport({
-      latitude: lat,
-      longitude: lon,
-      zoom: 12,
-      transitionInterpolator: new FlyToInterpolator({ speed: 1.0 }),
-      transitionDuration: "auto",
-      width: "100%",
-      height: "100vh",
-    });
-  }, [lat, lon]);
-
   return (
     <div className="App">
 
     </div>
   );
-}
+};
 
 export default App;
