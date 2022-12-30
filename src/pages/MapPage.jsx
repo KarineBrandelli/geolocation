307831addaafda3491c8b3901467060ce541b07d
Karine Brandelli <karinebrandelli@live.com>
2022-12-30 14:14:20 -0300

    build: create function to show map on screen

--- src/pages/MapPage.jsx
+++ src/pages/MapPage.jsx
@@ -1,13 +1,32 @@
 import React from "react";
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
 
 import "./MapPage.css";
 
 const MapPage = () => {
-  return (
-    <div className="map">
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: "AIzaSyDo_RGclqrLYasc0emSCPVquPmrLcUr4gk"
+  });
 
+  return (    
+    <div>
+      {isLoaded ? (
+        <GoogleMap
+          mapContainerStyle={{
+            width: '100%',
+            height: '100%'
+          }}
+          center={{
+            lat: -30.039101, 
+            lng: -51.203219
+          }}
+          zoom={15} >
+        </GoogleMap>
+      ) : ( 
+        <></>
+      )}
     </div>
-  );
-};
+  )};
 
 export default MapPage;
