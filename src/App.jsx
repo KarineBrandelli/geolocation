f9412c3a00c16ba1052e26b0acd396202c4b5f74
Karine Brandelli <karinebrandelli@live.com>
2023-02-02 13:27:42 -0300

    build: change file extension

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import MapPage from "./page/MapPage";
+
+export const REACT_APP_GOOGLE_API_KEY = "AIzaSyDo_RGclqrLYasc0emSCPVquPmrLcUr4gk";
+
+function App() {
+  return <MapPage />;
+}
+
+export default App;
